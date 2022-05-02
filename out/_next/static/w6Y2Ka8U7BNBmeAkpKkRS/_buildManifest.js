7d0fb373d602498f4ce352e5057fb12c3418b1e0
junyeongCHOI <asxd153@gmail.com>
2022-05-03 03:04:03 +0900

    add static

---
+++ out/_next/static/w6Y2Ka8U7BNBmeAkpKkRS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/dedeb474344d8310.css","static/chunks/pages/index-3df9c3ca845999fa.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
